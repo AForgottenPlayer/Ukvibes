b48fbaa1a056a84357f44bbfa38f1f03005e44a7
AForgottenPlayer <826lhalways@gmail.com>
2022-05-09 13:10:46 +0100

    2.1

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optionallo
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
